cc3c2817fb02c0db75a116d65c3812bc41b2fc99
rizki140699 <rizkimaulana140699@gmail.com>
2022-10-11 19:17:27 +0700

    merge conflix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
-<<<<<<< HEAD
 import { AdminComponent } from './admin/admin.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
-=======
->>>>>>> 898235e496d8c8978cf05b161f7a33530eb92a82
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     DashboardComponent,
-<<<<<<< HEAD
     RegisterComponent,
     AdminComponent,
     HeaderComponent,
     SidebarComponent,
     ContentComponent,
     FooterComponent
-=======
-    RegisterComponent
->>>>>>> 898235e496d8c8978cf05b161f7a33530eb92a82
   ],
   imports: [
     BrowserModule,
